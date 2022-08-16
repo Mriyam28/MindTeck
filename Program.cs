530ff16c1b659a34d45cc05fda999d9555dbf019
Mriyam28 <107410737+Mriyam28@users.noreply.github.com>
2022-08-16 15:11:48 +0530

    Add files via upload
    reverse a string

--- Program.cs
+++ Program.cs
@@ -8,20 +8,33 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            int y = 25;
-            double d = 5.234D;
-            bool b = true;
-            char c = 'y';
-            Console.WriteLine(Convert.ToString(x));
-            Console.WriteLine(Convert.ToDouble(y));
-            Console.WriteLine(Convert.ToBoolean(y));
-            Console.WriteLine(Convert.ToInt64(d));
-            Console.WriteLine(Convert.ToInt64(b));
-            Console.WriteLine(Convert.ToString(b));
-            Console.WriteLine(Convert.ToChar(y));
-            Console.WriteLine(Convert.ToInt32(c));
-            
+            /* //int x = 10;
+             //int y = 25;
+             double d = 5.234D;
+             bool b = true;
+             char c = 'y';
+             //Console.WriteLine(Convert.ToString(x));
+             Console.WriteLine(Convert.ToDouble(y));
+             Console.WriteLine(Convert.ToBoolean(y));
+             Console.WriteLine(Convert.ToInt64(d));
+             Console.WriteLine(Convert.ToInt64(b));
+             Console.WriteLine(Convert.ToString(b));
+             Console.WriteLine(Convert.ToChar(y));
+             Console.WriteLine(Convert.ToInt32(c));*/
+
+            string word = "Mindteck";
+
+            string w = "";
+
+            for (int i=word.Length-1; i>=0; i--)
+            {
+                w = w+word[i];
+                              
+             }
+            Console.WriteLine(w);
+
+
+
         }
 
         
